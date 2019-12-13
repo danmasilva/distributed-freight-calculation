5856c7d6c0f2f5076b091dde2c7a6911827b4900
Jefferson Freitas Oliveira <jefferson.oliveira@cedrofinances.com.br>
2019-12-13 14:01:20 -0300

    more validations

--- server/src/main/java/com/sd/dfc/service/PricingService.java
+++ server/src/main/java/com/sd/dfc/service/PricingService.java
@@ -1,5 +1,6 @@
 package com.sd.dfc.service;
 
+import com.google.common.math.BigIntegerMath;
 import com.sd.dfc.controller.DataControllerTransportadoraImpl;
 import com.sd.dfc.model.Transportadora;
 import com.sd.grpc.Business;
@@ -7,10 +8,13 @@ import com.sd.grpc.Business.PrecoRequest;
 import com.sd.grpc.Business.PrecoReturn;
 import com.sd.grpc.pricingGrpc.pricingImplBase;
 import io.grpc.stub.StreamObserver;
+import org.testng.Assert;
+
+import java.math.BigInteger;
 import java.util.List;
 
 public class PricingService extends pricingImplBase {
-    public final double TAXA_SERVICO = 1.2;
+    private final double TAXA_SERVICO = 1.2;
 
     DataControllerTransportadoraImpl dataControllerTransportadora = new DataControllerTransportadoraImpl();
     @Override
@@ -21,26 +25,48 @@ public class PricingService extends pricingImplBase {
         int cep = (int) request.getCep();
         double peso = request.getPeso();
 
-        if (cep == 0 || peso == 0.0 ) {
+
+        if (cep == 0 || peso == 0.0  || cep < 0 || peso < 0.0 ) {
             Business.PrecoReturn.Builder response = Business.PrecoReturn.newBuilder();
-            response.setResponseCode(400).setResponsemessage("Bad Request");
-            responseObserver.onNext(response.build());
-            responseObserver.onCompleted();
+            returnResponse(responseObserver, response, 400, "Bad Request");
             return;
         }
         String query = "readall transportadora";
+        Business.PrecoReturn.Builder response = Business.PrecoReturn.newBuilder();
         List<Transportadora> transportadoras = (List<Transportadora>) dataControllerTransportadora.readAll(query.split(" "));
+        isNullOrEmptyTransportadora(responseObserver, response, transportadoras);
+
+        boolean emptyResponse=true;
         for (Transportadora t : transportadoras) {
-            if(t.getAbrangencia().getCepInicio() < cep && t.getAbrangencia().getCepFim()>cep) {
-                Business.PrecoReturn.Builder response = Business.PrecoReturn.newBuilder();
-                response.setResponseCode((int) 200).setResponsemessage("OK");
+            if(t.getAbrangencia().getCepInicio() <= cep && t.getAbrangencia().getCepFim() >= cep) {
                 response.setTransportadora(t.getNome());
                 response.setPrice((double) t.getPeso() * TAXA_SERVICO);
-
                 responseObserver.onNext(response.build());
+                emptyResponse=false;
             }
-            
         }
+        if(emptyResponse) {
+            returnResponse(responseObserver, response, 200, "OK. Não ha transportadora para esse CEP");
+        } else {
+            returnResponse(responseObserver, response, 200, "OK.");
+        }
+
+    }
+
+    private void isNullOrEmptyTransportadora(StreamObserver<PrecoReturn> responseObserver, PrecoReturn.Builder response, List<Transportadora> transportadoras) {
+        if(transportadoras.isEmpty()) {
+            returnResponse(responseObserver, response, 200, "OK. Não ha transportadora na base.");
+            return;
+        }
+        if(transportadoras == null){
+            returnResponse(responseObserver, response, 500, "Internal Server Error.");
+            return;
+        }
+    }
+
+    private void returnResponse(StreamObserver<PrecoReturn> responseObserver, PrecoReturn.Builder response, int i, String s) {
+        response.setResponseCode(i).setResponsemessage(s);
+        responseObserver.onNext(response.build());
         responseObserver.onCompleted();
     }
 }
